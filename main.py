038bacdcd3be6bb15ae89f63b064c197e283e92f
Patrik Nordlund <130709148+epatnor@users.noreply.github.com>
2025-07-10 14:56:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from memory import init_db
 from dream_engine import generate_dream
 from reflector import reflect_on_latest_dream
 
-
 if __name__ == "__main__":
     init_db()
     generate_dream()
